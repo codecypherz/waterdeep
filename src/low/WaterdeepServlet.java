7a8ff4bab5bc35078063d24fd140139274435348
James Deyerle <james.deyerle@gmail.com>
2013-03-24 20:41:57 -0700

    Project setup.

---
+++ src/low/WaterdeepServlet.java
@@ -0,0 +1,11 @@
+package low;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class WaterdeepServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
